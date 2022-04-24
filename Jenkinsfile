68094aed2842d99e04e12a1377813ec96e831ddc
Vikas Bahuguna <vikasedu10@gmail.com>
2022-04-25 03:52:02 +0530

    Add mvn -version, package command

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,6 +3,10 @@ def gv
 pipeline {
     agent any
 
+    tools {
+        maven "maven-3"
+    }
+
     stages {
         stage("Init") {
             steps {
--- script.groovy
+++ script.groovy
@@ -1,13 +1,17 @@
 def testApp() {
     echo "Testing app"
+    sh "mvn -v"
+    echo "Test finished"
 }
 
 def buildApp() {
     echo "building app"
+    sh "mvn package"
 }
 
 def deployApp() {
     echo "Deploy app"
+    sh "deploy about to happen"
 }
 
 return this
