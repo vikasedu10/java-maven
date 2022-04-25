a076f2b40141da62b0f3fcf13efc47f084ead6f9
Vikas Bahuguna <vikasedu10@gmail.com>
2022-04-26 00:24:22 +0530

    Add mvn test, Deploy app

--- script.groovy
+++ script.groovy
@@ -1,18 +1,18 @@
 def testApp() {
     echo "Testing app"
     sh "mvn -v"
+    sh "mvn test"
     echo "Test finished"
 }
 
 def buildApp() {
-    echo "building app"
-    sh "mvn package"
+    echo "Building app"
+    sh "mvn -B -DskipTests clean package"
 }
 
 def deployApp() {
-    echo "Deploy app"
-    echo "deploy about to happen"
-    def dockerCmd = "docker run -p 3080:3080 -d nanajanashia/demo-app:1.0"
+    echo "Deploying app"
+    def dockerCmd = "docker run vikas1412/java-maven:1.0"
     sshagent(['ssh-keypair']) {
         sh "ssh -o StrictHostKeyChecking=no ec2-user@13.235.78.154 ${dockerCmd}"
     }
